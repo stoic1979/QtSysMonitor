8c0524b423c0c088a561e8f3d8cbb38249216294
Navjot Singh <weavebytes@gmail.com>
2016-11-23 19:04:10 +0530

    set main window title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,6 +38,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("Qt System Monitor");
     createMenus();
 
     SystemMonitor sm;

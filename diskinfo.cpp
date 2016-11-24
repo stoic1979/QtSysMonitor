aa3969bece142c78d45c7d7fd887015f60da6440
Navjot Singh <weavebytes@gmail.com>
2016-11-24 14:37:49 +0530

    documented code

--- diskinfo.cpp
+++ diskinfo.cpp
@@ -4,11 +4,18 @@
 
 DiskInfo::DiskInfo(QObject *parent) : QObject(parent)
 {
-
+    // pass
 }
 
-
+/**
+ * @brief DiskInfo::getDisksInfo
+ *
+ * Function will fetch disks info
+ * and emit signal with disks info
+ */
 void DiskInfo::getDisksInfo() {
 
     qDebug() << "[DiskInfo] getting disks' info...";
+
+    // ToDo: emit signal with disks info
 }

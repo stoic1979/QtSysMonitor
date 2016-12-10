d99ac0c87049cd23c9cd261b0897ffdb8a64ea1b
Navjot Singh <weavebytes@gmail.com>
2016-12-10 23:02:35 +0530

    singleton logger creation

--- qfilelogger.cpp
+++ qfilelogger.cpp
@@ -1,6 +1,21 @@
+#include <QDebug>
+
 #include "qfilelogger.h"
 
-QFileLogger::QFileLogger()
-{
+static QFileLogger* instance;
 
+QFileLogger::QFileLogger(QString filepath) : filepath(filepath),  file(filepath)
+{
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        qDebug() << "[QFileLogger] :: failed to open log file";
+    }
+}
+
+
+QFileLogger* QFileLogger::createLogger(QString filepath) {
+    if(!instance) {
+        instance = new QFileLogger(filepath);
+    }
+
+    return instance;
 }
--- qfilelogger.h
+++ qfilelogger.h
@@ -1,14 +1,29 @@
 #ifndef QFILELOGGER_H
 #define QFILELOGGER_H
 
+#include <QString>
+#include <QFile>
+
 /**
  * @brief The QFileLogger class
+ *
  * Class for logging logs of various levels - debug, info etc in log file.
+ *
+ * This logger is desinged to be a singleton for an application.
  */
-class QFileLogger
-{
+class QFileLogger {
 public:
-    QFileLogger();
+    static QFileLogger* createLogger(QString filepath);
+
+
+private:
+    QFileLogger(QString filepath);
+
+private:
+    QString     filepath;
+    QFile       file;
+
+
 };
 
 #endif // QFILELOGGER_H

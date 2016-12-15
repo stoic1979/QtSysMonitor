623fde93bb1e92001f10ffaa0c109c95ebfee753
Navjot Singh <weavebytes@gmail.com>
2016-12-16 00:36:43 +0530

    added some gpl comments in source files

--- aboutdialog.h
+++ aboutdialog.h
@@ -1,3 +1,28 @@
+/*
+ * -----------------------------------
+ * QtSysMonitor
+ * -----------------------------------
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------------
+ * Started By: Navjot Singh <weavebytes@gmail.com> in November 2016
+ *
+ * Organization:Weavebytes Infotech Pvt Ltd
+ * ---------------------------------------------------------------------------
+ */
+
 #ifndef ABOUTDIALOG_H
 #define ABOUTDIALOG_H
 
